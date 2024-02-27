56c2b30b21e6a7e71582f780dd9de3caacd5d97e
julie_c <forechecker@gmail.com>
2024-02-26 21:08:31 -0700

    final tweaks with html css

--- js/script.js
+++ js/script.js
@@ -1,7 +1,20 @@
-//make sure Bill input does not take letters
+const billInput = document.getElementById('bill');
+const peopleInput = document.getElementById('people');
+
+const resetBtn = document.getElementById('reset');
+
+//make sure Bill and people input does not take letters
 document.addEventListener('DOMContentLoaded', function () {
-  var billInput = document.getElementById('bill');
   billInput.addEventListener('input', function () {
     this.value = this.value.replace(/[^0-9.]/g, '');
   });
+
+  peoplInput.addEventListener('input', function () {
+    this.value = this.value.replace(/[^0-9.]/g, '');
+  });
+});
+
+resetBtn.addEventListener('click', function () {
+  billInput.value = 0;
+  peopleInput.value = 0;
 });
